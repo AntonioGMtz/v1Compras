6840c23fc1becd332ae4197064019040caadcf07
Antonio <antoniogallegos086@gmail.com>
2023-05-31 23:59:53 -0600

    Validating fields in add provider-check

--- resources/views/profile/agregarproov.blade.php
+++ resources/views/profile/agregarproov.blade.php
@@ -93,52 +93,148 @@
   <table>
   <tr>
     <td><label for="razonsocial">Razón Social:</label></td>
-    <td><input type="text" id="razonsocial" name="razonsocial"></td>
+    <td><input type="text" id="razonsocial" name="razonsocial" class="form-control {{ $errors->has('razonsocial') ? 'is-invalid' : '' }}" value="{{ old('razonsocial') }}">
+    @if ($errors->has('razonsocial'))
+    <div class="invalid-feedback">
+        {{ $errors->first('razonsocial') }}
+    </div>
+    @endif
+    </td>
     <td><label for="email">Email:</label></td>
-    <td><input type="email" id="email" name="email"></td>
+    <td><input type="email" id="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" value="{{ old('email') }}">
+    @if ($errors->has('email'))
+    <div class="invalid-feedback">
+        {{ $errors->first('email') }}
+    </div>
+    @endif
+    </td>
     <td><label for="nombrecomercial">Nombre Comercial:</label></td>
-    <td><input type="text" id="nombrecomercial" name="nombrecomercial"></td>
+    <td><input type="text" id="nombrecomercial" name="nombrecomercial" class="form-control {{ $errors->has('nombrecomercial') ? 'is-invalid' : '' }}" value="{{ old('nombrecomercial') }}">
+    @if ($errors->has('nombrecomercial'))
+    <div class="invalid-feedback">
+        {{ $errors->first('nombrecomercial') }}
+    </div>
+    @endif
+    </td>
   </tr>
   <tr>
 
     <td><label for="telefono1">Teléfono 1:</label></td>
-    <td><input type="text" id="teluno" name="teluno"></td>
+    <td><input type="text" id="teluno" name="teluno" class="form-control {{ $errors->has('teluno') ? 'is-invalid' : '' }}" value="{{ old('teluno') }}">
+    @if ($errors->has('teluno'))
+    <div class="invalid-feedback">
+        {{ $errors->first('teluno') }}
+    </div>
+    @endif
+    </td>
     <td><label for="teldos">Teléfono 2:</label></td>
-    <td><input type="text" id="teldos" name="teldos"></td>
+    <td><input type="text" id="teldos" name="teldos" class="form-control {{ $errors->has('teldos') ? 'is-invalid' : '' }}" value="{{ old('teldos') }}">
+    @if ($errors->has('teldos'))
+    <div class="invalid-feedback">
+        {{ $errors->first('teldos') }}
+    </div>
+    @endif
+    </td>
     <td><label for="rfc">RFC:</label></td>
-    <td><input type="text" id="rfc" name="rfc"></td>
+    <td><input type="text" id="rfc" name="rfc" class="form-control {{ $errors->has('rfc') ? 'is-invalid' : '' }}" value="{{ old('rfc') }}">
+    @if ($errors->has('rfc'))
+    <div class="invalid-feedback">
+        {{ $errors->first('rfc') }}
+    </div>
+    @endif
+    </td>
   </tr>
 
   <tr>
     <td><label for="clabe">Cuenta CLABE:</label></td>
-    <td><input type="text" id="clabe" name="clabe"></td>
+    <td><input type="text" id="clabe" name="clabe" class="form-control {{ $errors->has('clabe') ? 'is-invalid' : '' }}" value="{{ old('clabe') }}">
+    @if ($errors->has('clabe'))
+    <div class="invalid-feedback">
+        {{ $errors->first('clabe') }}
+    </div>
+    @endif
+    </td>
     <td><label for="sucursal">Sucursal:</label></td>
-    <td><input type="text" id="sucursal" name="sucursal"></td>
+    <td><input type="text" id="sucursal" name="sucursal" class="form-control {{ $errors->has('sucursal') ? 'is-invalid' : '' }}" value="{{ old('sucursal') }}">
+    @if ($errors->has('sucursal'))
+    <div class="invalid-feedback">
+        {{ $errors->first('sucursal') }}
+    </div>
+    @endif
+    </td>
     <td><label for="pais">Pais:</label></td>
-    <td><input type="text" id="pais" name="pais"></td>
+    <td><input type="text" id="pais" name="pais" class="form-control {{ $errors->has('pais') ? 'is-invalid' : '' }}" value="{{ old('pais') }}">
+    @if ($errors->has('pais'))
+    <div class="invalid-feedback">
+        {{ $errors->first('pais') }}
+    </div>
+    @endif
+    </td>
   </tr>
   <tr>
  
     <td><label for="estado">Estado:</label></td>
-    <td><input type="text" id="estado" name="estado"></td>
+    <td><input type="text" id="estado" name="estado" class="form-control {{ $errors->has('estado') ? 'is-invalid' : '' }}" value="{{ old('estado') }}">
+    @if ($errors->has('estado'))
+    <div class="invalid-feedback">
+        {{ $errors->first('estado') }}
+    </div>
+    @endif
+    </td>
     <td><label for="municipio">Municipio:</label></td>
-    <td><input type="text" id="municipio" name="municipio"></td>
+    <td><input type="text" id="municipio" name="municipio" class="form-control {{ $errors->has('municipio') ? 'is-invalid' : '' }}" value="{{ old('municipio') }}">
+    @if ($errors->has('municipio'))
+    <div class="invalid-feedback">
+        {{ $errors->first('municipio') }}
+    </div>
+    @endif
+    </td>
     <td><label for="localidad">Localidad:</label></td>
-    <td><input type="text" id="localidad" name="localidad"></td>
+    <td><input type="text" id="localidad" name="localidad" class="form-control {{ $errors->has('localidad') ? 'is-invalid' : '' }}" value="{{ old('localidad') }}">
+    @if ($errors->has('localidad'))
+    <div class="invalid-feedback">
+        {{ $errors->first('localidad') }}
+    </div>
+    @endif
+    </td>
   </tr>
 
   <tr>
     <td><label for="calle">Calle:</label></td>
-    <td><input type="text" id="calle" name="calle"></td>
+    <td><input type="text" id="calle" name="calle" class="form-control {{ $errors->has('calle') ? 'is-invalid' : '' }}" value="{{ old('calle') }}">
+    @if ($errors->has('calle'))
+    <div class="invalid-feedback">
+        {{ $errors->first('calle') }}
+    </div>
+    @endif
+    </td>
     <td><label for="colonia">Colonia:</label></td>
-    <td><input type="text" id="colonia" name="colonia"></td>
+    <td><input type="text" id="colonia" name="colonia" class="form-control {{ $errors->has('colonia') ? 'is-invalid' : '' }}" value="{{ old('colonia') }}">
+    @if ($errors->has('colonia'))
+    <div class="invalid-feedback">
+        {{ $errors->first('colonia') }}
+    </div>
+    @endif
+    </td>
     <td><label for="cp">Codigo postal:</label></td>
-    <td><input type="text" id="cp" name="cp"></td>
+    <td><input type="text" id="cp" name="cp" class="form-control {{ $errors->has('cp') ? 'is-invalid' : '' }}" value="{{ old('cp') }}">
+    @if ($errors->has('colonia'))
+    <div class="invalid-feedback">
+        {{ $errors->first('colonia') }}
+    </div>
+    @endif
+    </td>
   </tr>
   <tr>
   
     <td><label for="numint">Numero interior:</label></td>
-    <td><input type="text" id="numint" name="numint"></td>
+    <td><input type="text" id="numint" name="numint" class="form-control {{ $errors->has('numint') ? 'is-invalid' : '' }}" value="{{ old('numint') }}">
+    @if ($errors->has('numint'))
+    <div class="invalid-feedback">
+        {{ $errors->first('numint') }}
+    </div>
+    @endif
+    </td>
 
   </tr>
     </table>
